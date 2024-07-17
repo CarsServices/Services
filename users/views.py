180648720b59aed5b4ade5a90cd3ca0248cb62e8
Desond Anane <doanane@st.ug.edu.gh>
2024-07-17 23:30:07 +0000

    added the login page with a logo i designed for the site

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 # from django.http import HttpResponse
 
 # Create your views here.
 def login_view(request): # I'm going to create a function that will be responsible for rendering the login and handling the functionality
-    return render(request, "basic/login.html")
+    login_form = AuthenticationForm() #setting the login_form so that it can have te AuthenticationForm passed through it
+    return render(request, "basic/login.html", {'login_form':login_form})
