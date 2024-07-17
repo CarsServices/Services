4d47c938b9514d3bad3e9d21a7c254daa68d9149
Desond Anane <doanane@st.ug.edu.gh>
2024-07-17 04:56:27 +0000

    made changes to the profle setup

--- autocarmax/settings.py
+++ autocarmax/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'localflavor',
     'main',
     'users',
 ]
@@ -112,8 +111,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'       
+# Media
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL= '/media/'
 # added manually
 STATICFILES_DIRS = [
     BASE_DIR / "static",
