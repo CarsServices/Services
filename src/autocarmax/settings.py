3efaa7ca9967652406db6e29b5fa5668a32f0a95
Desond Anane <doanane@st.ug.edu.gh>
2024-07-26 03:54:20 +0000

    fixing minor bugs

--- src/autocarmax/settings.py
+++ src/autocarmax/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
+from django.contrib.messages import  constants as messages
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x_8g28d7m(_jafqkj5#g)iblw1dwc_r9tuo=!8_awxtrsql(&n'
+SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-x_8g28d7m(_jafqkj5#g)iblw1dwc_r9tuo=!8_awxtrsql(&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+# DEBUG = True
+DEBUG = config('DEBUG', cast= bool)
 ALLOWED_HOSTS = []
 
 # Application definition
@@ -98,8 +101,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# login settings
 LOGIN_REDIRECT_URL='/home/'
 LOGIN_URL= '/login/'
+
+# messages settings
+MESSAGE_TAGS ={
+    messages.ERROR: 'danger',
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
@@ -117,7 +127,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # Media
-import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL= '/media/'
 
